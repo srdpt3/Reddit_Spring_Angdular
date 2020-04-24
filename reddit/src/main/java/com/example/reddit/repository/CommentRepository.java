3f8affec767c8b1af82b7cdbcb91a00b0d2a601e
Dustin Yang <srdpt3@gmail.com>
2020-04-23 21:52:00 -0400

    implements comments

--- reddit/src/main/java/com/example/reddit/repository/CommentRepository.java
+++ reddit/src/main/java/com/example/reddit/repository/CommentRepository.java
@@ -5,9 +5,11 @@ import com.example.reddit.model.Comment;
 import com.example.reddit.model.Post;
 import com.example.reddit.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
+@CrossOrigin("http://localhost:4100/api/comments")
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByPost(Post post);
 
